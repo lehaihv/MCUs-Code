fd61e261a5ec200496c3d042a1eeb0d7e85ae0b2
Hai LE <69892340+lehaihv@users.noreply.github.com>
2024-11-17 10:12:33 -0500

    T460s

---
+++ ESP-IDF/RTOS/main/main.cpp
@@ -0,0 +1,129 @@
+/* #include "Arduino.h"
+
+extern "C" void app_main()
+{
+    initArduino();
+    pinMode(4, OUTPUT);
+    digitalWrite(4, HIGH);
+    // Do your own thing
+} */
+
+//file: main.cpp
+/* #include "Arduino.h"
+
+extern "C" void app_main()
+{
+  initArduino();
+
+  // Arduino-like setup()
+  Serial.begin(115200);
+  while(!Serial){
+    ; // wait for serial port to connect
+  }
+
+  // Arduino-like loop()
+  while(true){
+    Serial.println("loop");
+  }
+
+  // WARNING: if program reaches end of function app_main() the MCU will restart.
+}
+ */
+//file: main.cpp
+#include "Arduino.h"
+#include <Wire.h>
+#include <PID_v1.h>
+#include <Adafruit_MAX31865.h>
+
+// #define PIN_INPUT 0
+#define PIN_OUTPUT 6
+
+//Define Variables we'll be connecting to
+double Setpoint, Input, Output;
+
+//Specify the links and initial tuning parameters
+double Kp=10, Ki=0.5, Kd=1; //Kp=2, Ki=5, Kd=1;
+PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+
+//
+
+// Use software SPI: CS, DI, DO, CLK
+Adafruit_MAX31865 thermo = Adafruit_MAX31865(14, 13, 12, 11);
+// use hardware SPI, just pass in the CS pin
+//Adafruit_MAX31865 thermo = Adafruit_MAX31865(10);
+
+// The value of the Rref resistor. Use 430.0 for PT100 and 4300.0 for PT1000
+#define RREF      430.0
+// The 'nominal' 0-degrees-C resistance of the sensor
+// 100.0 for PT100, 1000.0 for PT1000
+#define RNOMINAL  100.0
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Adafruit MAX31865 PT100 Sensor Test!");
+  thermo.begin(MAX31865_2WIRE);  // set to 2WIRE or 4WIRE as necessary
+   //initialize the variables we're linked to
+  Input = thermo.temperature(RNOMINAL, RREF); //analogRead(PIN_INPUT);
+  Setpoint = 45;
+  //turn the PID on
+  myPID.SetMode(AUTOMATIC);
+}
+
+
+void loop() {
+  /* uint16_t rtd = thermo.readRTD();
+
+  Serial.print("RTD value: "); Serial.println(rtd);
+  float ratio = rtd;
+  ratio /= 32768;
+  Serial.print("Ratio = "); Serial.println(ratio,8);
+  Serial.print("Resistance = "); Serial.println(RREF*ratio,8); */
+  //Serial.print("Temperature = "); Serial.println(thermo.temperature(RNOMINAL, RREF));
+
+ /*  // Check and print any faults
+  uint8_t fault = thermo.readFault();
+  if (fault) {
+    Serial.print("Fault 0x"); Serial.println(fault, HEX);
+    if (fault & MAX31865_FAULT_HIGHTHRESH) {
+      Serial.println("RTD High Threshold"); 
+    }
+    if (fault & MAX31865_FAULT_LOWTHRESH) {
+      Serial.println("RTD Low Threshold"); 
+    }
+    if (fault & MAX31865_FAULT_REFINLOW) {
+      Serial.println("REFIN- > 0.85 x Bias"); 
+    }
+    if (fault & MAX31865_FAULT_REFINHIGH) {
+      Serial.println("REFIN- < 0.85 x Bias - FORCE- open"); 
+    }
+    if (fault & MAX31865_FAULT_RTDINLOW) {
+      Serial.println("RTDIN- < 0.85 x Bias - FORCE- open"); 
+    }
+    if (fault & MAX31865_FAULT_OVUV) {
+      Serial.println("Under/Over voltage"); 
+    }
+    thermo.clearFault();
+  }
+  Serial.println();
+  delay(1000); */
+
+  Input = thermo.temperature(RNOMINAL, RREF); //analogRead(PIN_INPUT);
+  myPID.Compute();
+  analogWrite(PIN_OUTPUT, Output);
+  Serial.println(thermo.temperature(RNOMINAL, RREF));
+  //Serial.println();
+  //delay(1000);
+}
+
+
+/* void setup(){
+  Serial.begin(115200);
+  while(!Serial){
+    ; // wait for serial port to connect
+  }
+}
+
+void loop(){
+    Serial.println("loop");
+    delay(1000);
+} */
