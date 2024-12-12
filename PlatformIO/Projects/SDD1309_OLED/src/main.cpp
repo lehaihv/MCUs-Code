e59b8e0c9971116357afd51ecf0f119491913c6f
Hai LE <69892340+lehaihv@users.noreply.github.com>
2024-12-12 15:51:44 -0500

    Update main.cpp

--- PlatformIO/Projects/SDD1309_OLED/src/main.cpp
+++ PlatformIO/Projects/SDD1309_OLED/src/main.cpp
@@ -55,12 +55,13 @@ void setup() {
   // Setting these values incorrectly may destroy your ADC!
   //                                                                ADS1015  ADS1115
   //                                                                -------  -------
-  // ads.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
+  ads.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
   // ads.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
   // ads.setGain(GAIN_TWO);        // 2x gain   +/- 2.048V  1 bit = 1mV      0.0625mV
   // ads.setGain(GAIN_FOUR);       // 4x gain   +/- 1.024V  1 bit = 0.5mV    0.03125mV
   // ads.setGain(GAIN_EIGHT);      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
   // ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
+  ads.setDataRate(RATE_ADS1115_64SPS);
 
   if (!ads.begin(0x48, &I2C_ADS1115)) {
     Serial.println("Failed to initialize ADS.");
