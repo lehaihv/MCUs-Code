e93b6ac79cf68a0f28d43e32b5d618f10d9a0f23
lehaihv <69892340+lehaihv@users.noreply.github.com>
2025-04-11 15:39:27 -0400

    Update main.cpp

--- PlatformIO/Projects/RTOS_T1/src/main.cpp
+++ PlatformIO/Projects/RTOS_T1/src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <stdlib.h>
 #include <Adafruit_NeoPixel.h> 
 
 #define PIN 38
@@ -7,7 +8,115 @@
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 #define DELAYVAL 1000
 
-// Our task: blink an LED
+// Settings
+static const uint8_t buf_len = 20;
+
+// Pins
+static const int led_pin = LED_BUILTIN;
+
+// Globals
+static int led_delay = 500;   // ms
+
+//*****************************************************************************
+// Tasks
+
+// Task: Blink LED at rate set by global variable
+void toggleLED(void *parameter) {
+  while (1) {
+    pixels.setPixelColor(0, pixels.Color(0, 0, 255));
+    pixels.show();
+    vTaskDelay(led_delay / portTICK_PERIOD_MS);
+    pixels.setPixelColor(0, pixels.Color(0, 255, 0));
+    pixels.show();
+    vTaskDelay(led_delay / portTICK_PERIOD_MS);
+    //Serial.println("Task 1");
+  }
+}
+
+// Task: Read from serial terminal
+// Feel free to use Serial.readString() or Serial.parseInt(). I'm going to show
+// it with atoi() in case you're doing this in a non-Arduino environment. You'd
+// also need to replace Serial with your own UART code for non-Arduino.
+void readSerial(void *parameters) {
+
+  char c;
+  char buf[buf_len];
+  uint8_t idx = 0;
+
+  // Clear whole buffer
+  memset(buf, 0, buf_len);
+
+  // Loop forever
+  while (1) {
+
+    // Read characters from serial
+    if (Serial.available() > 0) {
+      c = Serial.read();
+
+      // Update delay variable and reset buffer if we get a newline character
+      if (c == '\n') {
+        led_delay = atoi(buf);
+        Serial.print("Updated LED delay to: ");
+        Serial.println(led_delay);
+        memset(buf, 0, buf_len);
+        idx = 0;
+      } else {
+        
+        // Only append if index is not over message limit
+        if (idx < buf_len - 1) {
+          buf[idx] = c;
+          idx++;
+        }
+      }
+    }
+  }
+}
+
+//*****************************************************************************
+// Main
+
+void setup() {
+
+  // Configure pixels
+  pixels.begin();
+  pixels.setBrightness(3);
+  pixels.clear();
+
+  // Configure serial and wait a second
+  Serial.begin(115200);
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+  Serial.println("Multi-task LED Demo");
+  Serial.println("Enter a number in milliseconds to change the LED delay.");
+
+  // Start blink task
+  xTaskCreatePinnedToCore(  // Use xTaskCreate() in vanilla FreeRTOS
+            toggleLED,      // Function to be called
+            "Toggle LED",   // Name of task
+            1024,           // Stack size (bytes in ESP32, words in FreeRTOS)
+            NULL,           // Parameter to pass
+            1,              // Task priority
+            NULL,           // Task handle
+            1);       // Run on one core for demo purposes (ESP32 only)
+            
+  // Start serial read task
+  xTaskCreatePinnedToCore(  // Use xTaskCreate() in vanilla FreeRTOS
+            readSerial,     // Function to be called
+            "Read Serial",  // Name of task
+            1024,           // Stack size (bytes in ESP32, words in FreeRTOS)
+            NULL,           // Parameter to pass
+            1,              // Task priority (must be same to prevent lockup)
+            NULL,           // Task handle
+            1);       // Run on one core for demo purposes (ESP32 only)
+
+  // Delete "setup and loop" task
+  vTaskDelete(NULL);
+}
+
+void loop() {
+  // Execution should never get here
+}
+
+/*// Our task: blink an LED
 void toggleLED(void *parameter) {
   while(1) {
     pixels.setPixelColor(0, pixels.Color(0, 0, 255));
@@ -47,4 +156,94 @@ void loop() {
   
   // setup() and loop() run in their own task with priority 1 in core 1
   // on ESP32
+} */
+
+/* // Some string to print
+const char msg[] = "Barkadeer brig Arr booty rum.";
+
+// Task handles
+static TaskHandle_t task_1 = NULL;
+static TaskHandle_t task_2 = NULL;
+
+//*****************************************************************************
+// Tasks
+
+// Task: print to Serial Terminal with lower priority
+void startTask1(void *parameter) {
+
+  // Count number of characters in string
+  int msg_len = strlen(msg);
+
+  // Print string to Terminal
+  while (1) {
+    Serial.println();
+    for (int i = 0; i < msg_len; i++) {
+      Serial.print(msg[i]);
+    }
+    Serial.println();
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
 }
+
+// Task: print to Serial Terminal with higher priority
+void startTask2(void *parameter) {
+  while (1) {
+    Serial.print('*');
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+  }
+}
+
+//*****************************************************************************
+// Main (runs as its own task with priority 1 on core 1)
+ 
+void setup() {
+
+  // Configure Serial (go slow so we can watch the preemption)
+  Serial.begin(300);
+
+  // Wait a moment to start (so we don't miss Serial output)
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+  Serial.println();
+  Serial.println("---FreeRTOS Task Demo---");
+
+  // Print self priority
+  Serial.print("Setup and loop task running on core ");
+  Serial.print(xPortGetCoreID());
+  Serial.print(" with priority ");
+  Serial.println(uxTaskPriorityGet(NULL));
+
+ // Task to run forever
+  xTaskCreatePinnedToCore(startTask1,
+                          "Task 1",
+                          1024,
+                          NULL,
+                          1,
+                          &task_1,
+                          0);
+
+  // Task to run once with higher priority
+  xTaskCreatePinnedToCore(startTask2,
+                          "Task 2",
+                          1024,
+                          NULL,
+                          2,
+                          &task_2,
+                          0);
+}
+
+void loop() {
+
+  // Suspend the higher priority task for some intervals
+  for (int i = 0; i < 3; i++) {
+    vTaskSuspend(task_2);
+    vTaskDelay(2000 / portTICK_PERIOD_MS);
+    vTaskResume(task_2);
+    vTaskDelay(2000 / portTICK_PERIOD_MS);
+  }
+
+  // Delete the lower priority task
+  if (task_1 != NULL) {
+    vTaskDelete(task_1);
+    task_1 = NULL;
+  }
+} */
