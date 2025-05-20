238f19760e0346d4c1433703c583a366f8dd9e18
lehaihv <69892340+lehaihv@users.noreply.github.com>
2025-05-20 13:02:25 -0400

    Update main.cpp

--- PlatformIO/Projects/Pump/src/main.cpp
+++ PlatformIO/Projects/Pump/src/main.cpp
@@ -5,18 +5,24 @@
 #define IN2   11
 #define Valve 13
 
+#define buffer_time 5000
+#define water_time 2800
+#define air_time 2500
+
 int count = 0;
-int speed = 120;
+int speed = 120;  // around 4ml/min
+
+
 
 void setup() {
   // put your setup code here, to run once:
   //pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
   pinMode(Valve, OUTPUT);
-  // digitalWrite(IN1, LOW);
+  //digitalWrite(IN1, LOW);
   analogWrite(IN1, 0);
   digitalWrite(IN2, LOW);
-  digitalWrite(Valve, LOW);
+  digitalWrite(Valve, HIGH);
   // analogWrite(14, 250);
   Serial.begin(115200);
   //Serial.setDebugOutput(false);
@@ -28,11 +34,13 @@ void setup() {
 
 void pump_on() {
   analogWrite(IN1, speed);
+  //digitalWrite(IN1, HIGH);
 }
 
 void pump_off() {
-  //analogWrite(IN1, 0);
+  analogWrite(IN1, 0);
   //delay(100);
+  //digitalWrite(IN1, LOW);
 }
 
 void valve_water() {
@@ -46,6 +54,12 @@ void valve_air() {
 }
 
 void loop() {
+  // take the liqud to ready position
+  valve_water();
+  pump_on();
+  delay(12000);
+  pump_off();    
+  //while(1)
   // put your main code here, to run repeatedly:
   // take water to the pump input
  /*  while(1){
@@ -57,65 +71,65 @@ void loop() {
   } */
   valve_water();
   pump_on();
-  delay(10000);
+  delay(buffer_time);
   // stop pump
-  pump_off();
+  //pump_off();
   // valve air
   valve_air();
   // pump air buffer
-  pump_on();
-  delay(8000);
+  //pump_on();
+  delay(buffer_time);
   // stop pump
-  pump_off();
+  //pump_off();
   // valve water
   valve_water();
   
 
-  while(count < 15) { // 15 small wells
+  while(count < 50) { // 15 small wells
     // start cycle
     // pump 20 ul water and 10ul air 
     valve_water();
-    pump_on();
-    delay(1000); // 500us equal to ?ul
-    pump_off();
+    //pump_on();
+    delay(water_time); // 500us equal to ?ul
+    //pump_off();
     // valve air
     valve_air();
-    pump_on();
-    delay(1000); 
-    pump_off();
+    //pump_on();
+    delay(air_time); 
+    //pump_off();
     count++;
   }
 
   count = 0;
-  while(count < 20) { // 20 large wells 
+  while(count < 1) { // 20 large wells 
     // start cycle
     // pump 20 ul water and 10ul air 
     valve_water();
-    pump_on();
-    delay(1500); // 500us equal to ?ul
-    pump_off();
+    //pump_on();
+    delay(water_time*5); // 500us equal to ?ul
+    //pump_off();
     // valve air
     valve_air();
-    pump_on();
-    delay(1000); 
-    pump_off();
+    //pump_on();
+    delay(air_time); 
+    //pump_off();
     count++;
   }
   
   // pump water buffer
   valve_water();
-  pump_on();
-  delay(5000);
-  pump_off();
+  //pump_on();
+  delay(buffer_time);
+  //pump_off();
   // pump air buffer
   valve_air();
-  pump_on();
-  delay(10000);
-  pump_off();
+  //pump_on();
+  delay(buffer_time*2);
+  //pump_off();
   valve_water();
 
   valve_water();
-  analogWrite(IN1, 0);
+  pump_off();
 
   while(1){
     // stop pump, valve off
