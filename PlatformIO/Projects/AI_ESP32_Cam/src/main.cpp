a14378a8808047225b149cfb2b455840ea85e222
lehaihv <69892340+lehaihv@users.noreply.github.com>
2025-06-11 13:36:04 -0400

    Update main.cpp

--- PlatformIO/Projects/AI_ESP32_Cam/src/main.cpp
+++ PlatformIO/Projects/AI_ESP32_Cam/src/main.cpp
@@ -66,6 +66,9 @@ void setup() {
   Serial.begin(115200);
   Serial.println();
 
+  esp_task_wdt_init(60, true); // 60 second timeout, reset on timeout
+  esp_task_wdt_add(NULL);      // Add current thread to WDT
+
   // Connect to Wi-Fi
   WiFi.begin(ssid, password);
   Serial.print("Connecting to WiFi...");
@@ -162,8 +165,8 @@ void setup() {
   digitalWrite(4, LOW);
   rtc_gpio_hold_en(GPIO_NUM_4); // Hold the GPIO state during deep sleep
 
-  esp_task_wdt_init(60, true); // 60 second timeout, reset on timeout
-  esp_task_wdt_add(NULL);      // Add current thread to WDT
+  /* esp_task_wdt_init(60, true); // 60 second timeout, reset on timeout
+  esp_task_wdt_add(NULL);      // Add current thread to WDT */
 
   capturePhotoSaveLittleFS();
   sendPhoto();
